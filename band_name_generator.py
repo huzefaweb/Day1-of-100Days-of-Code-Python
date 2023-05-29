d7f0a8a65ba09cbe367bf6315efccb3806bc4791
Huzefa Ahmed <106472690+huzefaweb@users.noreply.github.com>
2023-05-29 08:05:15 +0500

    band_name_generator.py
    * demonstrates the print and input commands
    * first variables derived from the input
    * shows how to print what the variable contains and space it - note 
    * the \n which gives a new line

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+#---------DAY 1 Of 100 Days of Python----------
+print("Welcom To Band Name Generator")
+city_name=input("In which city did you grow up?\n")
+pet_name=input("What is the name of your pet?\n")
+# band_name=city_name + pet_name
+print("The name of your band could be: "+ city_name + " " + pet_name)
+# print("The name of your band could be: "+ band_name)
